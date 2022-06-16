44bd6c5e944310a49d5022bb0d0a3555b42ba0a2
Smangele47 <220047154@mycput.ac.za>
2022-06-16 17:14:09 +0200

    Commit by Smangele

--- src/test/java/za/ac/school_management/RespositoryTest/EmployeeRepositoryTest.java
+++ src/test/java/za/ac/school_management/RespositoryTest/EmployeeRepositoryTest.java
@@ -1,11 +1,11 @@
-/* EmployeeRepository.java
-   Author: Damone Hartnick
-   Student Number : 219093717
-   Date: June 2022
-*/
-package za.ac.school_management.RespositoryTest;
-
-
-public class EmployeeRepositoryTest {
-
-}
+///* EmployeeRepository.java
+//   Author: Damone Hartnick
+//   Student Number : 219093717
+//   Date: June 2022
+//*/
+//package za.ac.school_management.RespositoryTest;
+//
+//
+//public class EmployeeRepositoryTest {
+//
+//}
